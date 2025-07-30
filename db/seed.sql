aae15a67606010b0e1fbff764a653b423f80e758
jamief3105 <james.fraser3105@gmail.com>
2025-07-30 14:23:06 +0100

    schema and intitial seed creation

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO flood_severity (severity_level,severity_name, severity_meaning)
+    VALUES (1, 'Severe Flood Warning','Severe Flooding, Danger to Life.'),
+           (2,'Flood Warning','Flooding is Expected, Immediate Action Required.'),
+           (3,'Flood Alert', 'Flooding is Possible, Be Prepared.'),
+           (4,'	Warning no Longer in Force','The warning is no longer in force');
