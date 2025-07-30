fb56485fdfe10225f6d4f33c91be1695b2ff18e5
jamief3105 <james.fraser3105@gmail.com>
2025-07-30 16:40:47 +0100

    added on locations to seeds

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO flood_severity (severity_level,severity_name, severity_meaning)
     VALUES (1, 'Severe Flood Warning','Severe Flooding, Danger to Life.'),
            (2,'Flood Warning','Flooding is Expected, Immediate Action Required.'),
            (3,'Flood Alert', 'Flooding is Possible, Be Prepared.'),
-           (4,'	Warning no Longer in Force','The warning is no longer in force');
+           (4,'Warning no Longer in Force','The warning is no longer in force');
 
 
 INSERT INTO locations(location_name,latitude,longitude)
