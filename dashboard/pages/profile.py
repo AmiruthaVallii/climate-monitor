54ed0258e1cbcab09a41ee0f3e0e1d2eb4a74dc8
lindengraves <lgraves121@protonmail.com>
2025-08-07 09:56:49 +0100

    Added logging and implemented suggestions

--- dashboard/pages/profile.py
+++ dashboard/pages/profile.py
@@ -1,25 +1,27 @@
 # pylint: disable=import-error
 """Profile management page for the dashboard."""
 import os
+import logging
 import json
 import streamlit as st
-import requests as req
 import psycopg2
 import psycopg2.extras
-import pandas as pd
 import folium as fl
 from streamlit_folium import st_folium
 import boto3
 from login import get_conn  # pylint: disable=import-error
 from modules.nav import navbar
 
-LAMBDA_NAME = "c18-climate-monitor-new-location-orchestrator-lambda"
+NEW_LOCATION_LAMBDA = "c18-climate-monitor-new-location-orchestrator-lambda"
 
 boto3.setup_default_session(
     aws_access_key_id=os.getenv("MY_AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("MY_AWS_SECRET_ACCESS_KEY"),
     region_name=os.getenv("MY_AWS_REGION")
 )
+
+logging.basicConfig(
+    format="%(levelname)s | %(asctime)s | %(message)s", level=logging.INFO)
 
 
 def logout() -> None:
@@ -34,12 +36,15 @@ def get_users_locations():
     conn = get_conn()
     try:
         with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            cur.execute(("SELECT l.location_id, l.location_name, la.subscribe_to_alerts, la.subscribe_to_summary "
+            cur.execute(("SELECT l.location_id, l.location_name, "
+                         "la.subscribe_to_alerts, la.subscribe_to_summary "
                          "FROM locations as l "
                          "JOIN location_assignment AS la USING (location_id) "
                          "JOIN users AS u USING (user_id) "
                          "WHERE u.user_id=%s"),
                         (st.session_state["user_id"],))
+            logging.info("Gotten all location assignments for user_id %s",
+                         st.session_state["user_id"])
             return cur.fetchall()
     finally:
         conn.close()
@@ -73,12 +78,16 @@ def get_location_assignment(user_id: int, location_id: str) -> tuple:
                 data = {"subscribe_to_alerts": False,
                         "subscribe_to_summary": False,
                         "record_exists": False}
+            logging.info("Got location assignment for user_id %s, location_id %s",
+                         user_id, location_id)
             return data
     finally:
         conn.close()
 
 
-def update_location_assignment(user_id: int, location_id: int, record_exists: bool, get_alerts: bool, get_summary: bool) -> None:
+def update_location_assignment(user_id: int, location_id: int, record_exists: bool,
+                               get_alerts: bool, get_summary: bool) -> None:
+    """Update location assignment table for user user_id with location location_id"""
     conn = get_conn()
     try:
         cur = conn.cursor()
@@ -89,6 +98,8 @@ def update_location_assignment(user_id: int, location_id: int, record_exists: bo
                          "WHERE user_id=%s AND location_id=%s;"),
                         (user_id, location_id))
             conn.commit()
+            logging.info("Deleted location assignment for user_id %s, location_id %s",
+                         user_id, location_id)
             return None
         if record_exists:
             cur.execute(("UPDATE location_assignment "
@@ -98,12 +109,17 @@ def update_location_assignment(user_id: int, location_id: int, record_exists: bo
                          "AND location_id = %s;"),
                         (get_alerts, get_summary, user_id, location_id))
             conn.commit()
+            logging.info("Updated location assignment for user_id %s, location_id %s",
+                         user_id, location_id)
             return None
         cur.execute(("INSERT INTO location_assignment "
                      "(user_id, location_id, subscribe_to_alerts, subscribe_to_summary) "
                      "VALUES (%s, %s, %s, %s);"),
                     (user_id, location_id, get_alerts, get_summary))
         conn.commit()
+        logging.info("Inserted new location assignment for user_id %s, location_id %s",
+                     user_id, location_id)
+        return None
     finally:
         conn.close()
 
@@ -114,7 +130,6 @@ def map_get_notifications_callback(user_id: int, location_id: int, record_exists
         ("get_notifications", "alerts", location_id))
     get_summary = st.session_state.get(
         ("get_notifications", "summary", location_id))
-    conn = get_conn()
     update_location_assignment(
         user_id, location_id, record_exists, get_alerts, get_summary)
 
@@ -127,7 +142,7 @@ def insert_new_location(latitude: float, longitude: float) -> None:
         ("new_location_alerts", latitude, longitude))
     get_summary = st.session_state.get(
         ("new_location_summary", latitude, longitude))
-    if (not location_name) or not (location_name.isalpha()):
+    if (not location_name) or (not location_name.isalpha()):
         st.write("Error: characters in location name must be alphabetic.")
         return None
     conn = get_conn()
@@ -138,56 +153,67 @@ def insert_new_location(latitude: float, longitude: float) -> None:
                      "RETURNING location_id;"),
                     (location_name, latitude, longitude))
         location_id = cur.fetchone()[0]
-        st.write(location_id)
         conn.commit()
+        logging.info("Inserted %s into locations, location_id = %s",
+                     location_name, location_id)
         if get_alerts or get_summary:
             cur.execute(("INSERT INTO location_assignment "
                         "(user_id, location_id, subscribe_to_alerts, subscribe_to_summary) "
                          "VALUES (%s, %s, %s, %s);"),
                         (st.session_state.user_id, location_id, get_alerts, get_summary))
             conn.commit()
+            logging.info("Inserted new location assignment for user_id %s, location_id %s",
+                         st.session_state.user_id, location_id)
         lambda_client = boto3.client('lambda')
         response = lambda_client.invoke(
-            FunctionName=LAMBDA_NAME,
+            FunctionName=NEW_LOCATION_LAMBDA,
             InvocationType="Event",
             Payload=json.dumps({"location_id": location_id,
                                 "latitude": latitude,
                                 "longitude": longitude})
         )
-    except psycopg2.errors.UniqueViolation:
+        logging.info("Invoked %s. Status code: %s",
+                     NEW_LOCATION_LAMBDA,
+                     response['ResponseMetadata']['HTTPStatusCode'])
+        return None
+    except psycopg2.errors.UniqueViolation:  # pylint: disable=no-member
         st.write("Error: location name already exists, pick a new one.")
+        logging.info("User attempted to insert already existing location")
+        return None
     finally:
         conn.close()
 
 
-def notification_manager_callback(location: dict):
+def notification_manager_callback(location_dict: dict):
+    """Callback function for use with the notification manager form."""
     get_alerts = st.session_state.get(
-        ("notification_manage_form", location["location_id"]))
+        ("notification_manage_form", location_dict["location_id"]))
     get_summary = st.session_state.get(
-        ("notification_manager_form", location["location_id"])
+        ("notification_manager_form", location_dict["location_id"])
     )
     update_location_assignment(
-        st.session_state.user_id, location["location_id"], True, get_alerts, get_summary)
+        st.session_state.user_id, location_dict["location_id"], True, get_alerts, get_summary)
 
 
-def notification_manager_form(location: dict) -> None:
-    with st.form(location["location_name"],
+def notification_manager_form(location_dict: dict) -> None:
+    """Form to manage notifications"""
+    with st.form(location_dict["location_name"],
                  enter_to_submit=False,
                  border=False):
-        col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
-        with col1:
-            st.write(location["location_name"])
-        with col2:
-            alerts = st.checkbox(
-                "Alerts", value=location["subscribe_to_alerts"],
-                key=("notification_manage_form", location["location_id"]))
-        with col3:
-            summary = st.checkbox(
-                "Summary", value=location["subscribe_to_summary"],
-                key=("notification_manager_form", location["location_id"]))
-        with col4:
-            submitted = st.form_submit_button(
-                "Submit", on_click=notification_manager_callback, args=(location,))
+        form_col1, form_col2, form_col3, form_col4 = st.columns((1, 1, 1, 1))
+        with form_col1:
+            st.write(location_dict["location_name"])
+        with form_col2:
+            st.checkbox(
+                "Alerts", value=location_dict["subscribe_to_alerts"],
+                key=("notification_manage_form", location_dict["location_id"]))
+        with form_col3:
+            st.checkbox(
+                "Summary", value=location_dict["subscribe_to_summary"],
+                key=("notification_manager_form", location_dict["location_id"]))
+        with form_col4:
+            st.form_submit_button(
+                "Submit", on_click=notification_manager_callback, args=(location_dict,))
 
 
 if __name__ == "__main__":
@@ -271,23 +297,6 @@ if __name__ == "__main__":
                                   la.get("record_exists", True)))
                         if submitted:
                             st.write("Submitted")
-                        # la = get_location_assignment(
-                        #     st.session_state["user_id"],
-                        #     location_id_map[out["last_object_clicked_popup"]]
-                        # )
-                        # st.subheader(out["last_object_clicked_popup"])
-                        # alerts = st.checkbox(
-                        #     "Alerts", value=la["subscribe_to_alerts"],
-                        # )
-                        # summary = st.checkbox(
-                        #     "Summary", value=la["subscribe_to_summary"],
-                        # )
-                        # submitted = st.form_submit_button(
-                        #     "Submit")
-                        # if submitted:
-                        #     update_location_assignment(
-                        #         st.session_state.user_id, location_id_map[out["last_object_clicked_popup"]], la.get("record_exists", True), alerts, summary)
-                        #     st.write("Submitted")
                 else:
                     st.write("Click on a pin")
             with st.container(height=340):
@@ -300,7 +309,8 @@ if __name__ == "__main__":
                                  height='stretch'):
                         st.text(f"Coordinates: {lat:.2f}°N, {lon:.2f}°E")
                         name = st.text_input(
-                            "Location name:", placeholder="Enter location name", label_visibility="collapsed",
+                            "Location name:", placeholder="Enter location name",
+                            label_visibility="collapsed",
                             key=("new_location_name", lat, lon))
                         alerts = st.checkbox(
                             "Alerts", key=("new_location_alerts", lat, lon))
@@ -309,6 +319,6 @@ if __name__ == "__main__":
                         submitted = st.form_submit_button(
                             "Submit", on_click=insert_new_location, args=(lat, lon))
                         if submitted:
-                            # insert_new_location(
-                            #     name, lat, lon, alerts, summary)
                             st.write("Submitted")
+                else:
+                    st.write("Click on the map")
